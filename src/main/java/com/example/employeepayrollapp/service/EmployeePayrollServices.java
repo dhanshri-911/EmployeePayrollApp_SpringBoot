1f55f4501ff63e3daeb60bcc520b5959211e1563
Dhanshri911 <dszingade911@gmail.com>
2022-07-21 02:18:47 +0530

    Added:UC3 Storing Data

--- src/main/java/com/example/employeepayrollapp/service/EmployeePayrollServices.java
+++ src/main/java/com/example/employeepayrollapp/service/EmployeePayrollServices.java
@@ -9,32 +9,36 @@ import java.util.List;
 
 @Service
 public class EmployeePayrollServices {
+    List<EmployeePayrollData> employeeDataList = new ArrayList<>();
+
     public List<EmployeePayrollData> getEmployeePayrollData() {
-        List<EmployeePayrollData> employeeDataList = new ArrayList<>();
-        employeeDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Cech", 1000)));
+        //  employeeDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Cech", 1000)));
         return employeeDataList;
     }
 
     public EmployeePayrollData getEmployeePayrollDataById(int empID) {
-        EmployeePayrollData employeePayrollData;
-        employeePayrollData = new EmployeePayrollData(empID, new EmployeePayrollDTO("Amol", 5000));
-        return employeePayrollData;
+//    EmployeePayrollData employeePayrollData;
+//    employeePayrollData = new EmployeePayrollData(empID, new EmployeePayrollDTO("Amol", 5000));
+        return employeeDataList.get(empID-1);
     }
 
     public EmployeePayrollData addEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         EmployeePayrollData employeePayrollData;
-        employeePayrollData = new EmployeePayrollData(2, employeePayrollDTO);
+        employeePayrollData = new EmployeePayrollData(employeeDataList.size()+1, employeePayrollDTO);
+        employeeDataList.add(employeePayrollData);
         return employeePayrollData;
     }
 
     public EmployeePayrollData updateEmployeeData(int empID, EmployeePayrollDTO employeePayrollDTO) {
-        EmployeePayrollData employeePayrollData;
-        employeePayrollData = new EmployeePayrollData(empID, employeePayrollDTO);
+        EmployeePayrollData employeePayrollData = this.getEmployeePayrollDataById(empID);
+        employeePayrollData.setName(employeePayrollDTO.name);
+        employeePayrollData.setSalary(employeePayrollDTO.salary);
+        employeeDataList.set(empID-1, employeePayrollData);
         return employeePayrollData;
     }
 
     public void deleteEmployeePayrollData(int empID) {
-
+        employeeDataList.remove(empID-1);
     }
 
 }
