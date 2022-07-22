181e570bb33210a932f36cc969d1fca75d1f05a8
Dhanshri911 <dszingade911@gmail.com>
2022-07-22 09:58:04 +0530

    Added:UC3_DAY6 Adding Logging Properties

--- src/main/java/com/example/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/example/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,9 +1,10 @@
 package com.example.employeepayrollapp.model;
 
+
 import com.example.employeepayrollapp.DTO.EmployeePayrollDTO;
+import lombok.Data;
 
-
-
+@Data
 public class EmployeePayrollData {
     private int employeeID;
     private String name;
@@ -19,27 +20,27 @@ public class EmployeePayrollData {
     }
 
 
-    public int getEmployeeID() {
-        return employeeID;
-    }
-
-    public void setEmployeeID(int employeeID) {
-        this.employeeID = employeeID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
+//    public int getEmployeeID() {
+//        return employeeID;
+//    }
+//
+//    public void setEmployeeID(int employeeID) {
+//        this.employeeID = employeeID;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(int salary) {
+//        this.salary = salary;
+//    }
 }
